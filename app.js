56df7dbdebe257c6a8f997f1f2df5a216b7da235
Abdulahaditsol <abdul.ahad@itsolution24x7.com>
2020-03-17 16:42:14 +0500

    reset password

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const passwordRouter = require('./routes/resetPassword');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/', indexRouter);
 app.use('/api/user', usersRouter);
+app.use('/api/password', passwordRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
